efd780b910e7a20fe7be2c450aca4e17c5ee450b
jyriruohoniemi <77733793+jyriruohoniemi@users.noreply.github.com>
2025-01-23 18:11:25 +0200

    Debug III

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: "http://localhost:4200",
-    excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     viewportHeight: 1080,
     viewportWidth: 1920,
